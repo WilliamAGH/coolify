edf382f954b2aa8611761004d0be01798bbb7d15
William Callahan <william@aventure.vc>
2026-07-31 13:19:31 -0700

    chore(release): prepare 4.13.53-fork

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
     'coolify' => [
-        'version' => '4.13.52-fork',
+        'version' => '4.13.53-fork',
         'helper_version' => '1.0.14',
         'railpack_version' => '0.23.0',
         'self_hosted' => env('SELF_HOSTED', true),
